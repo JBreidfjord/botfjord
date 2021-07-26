49becf3beec54aa1a338b5e962b80726322ce041
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-07-26 11:21:51 -0600

    Defined Node functions

--- botfjord/mcts.py
+++ botfjord/mcts.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from chess import Board
+from chess import Board, Move
 
 
 @dataclass
@@ -13,17 +13,73 @@ class Branch:
 
 
 class Node:
-    def __init__(self, state: Board, priors, value, parent=None, last_move=None):
+    def __init__(
+        self,
+        state: Board,
+        priors: dict[Move, float],
+        value: float,
+        parent: Node = None,
+        last_move: Move = None,
+    ):
+        """
+        A Monte Carlo Tree Search node\n
+        Args:
+            state: Board object for current game state
+            priors: dict of float values for all legal moves from current state
+            value: The calculated value of the current state
+            parent: Optional; The Node leading to this Node
+            last_move: Optional; The move leading from parent state to current state
+        """
         self.state = state
         self.value = value
         self.parent = parent
         self.last_move = last_move
         self.total_visit_count = 1
-        self.branches = {}
-        self.children = {}
+        self.branches: dict[Move, Branch] = {}
+        self.children: dict[Move, Node] = {}
 
         for move in list(state.legal_moves):
             self.branches[move] = Branch(priors[move])
+
+    def moves(self):
+        """Returns a list of all possible moves from this node"""
+        return list(self.branches.keys())
+
+    def add_child(self, move: Move, child_node: Node):
+        """Adds a new child node\n
+        Raises exception if child exists"""
+        if self.children.get(move) is not None:
+            raise ValueError("Child already exists")
+        self.children[move] = child_node
+
+    def has_child(self, move: Move):
+        """Returns bool for existence of given child node"""
+        return move in self.children
+
+    def expected_value(self, move: Move):
+        """Returns expected value for given move\n
+        Expected value is the average value of past visits to that branch"""
+        branch = self.branches[move]
+        if branch.visit_count == 0:
+            return 0.0
+        return branch.total_value / branch.visit_count
+
+    def prior(self, move: Move):
+        return self.branches[move].prior
+
+    def visit_count(self, move: Move):
+        branch = self.branches.get(move)
+        return branch.visit_count if branch is not None else 0
+
+    def record_visit(self, move: Move, value: float):
+        self.branches[move].visit_count += 1
+        self.branches[move].total_value += value
+        self.total_visit_count += 1
+
+    def check_visit_counts(self, num_rounds: int):
+        visit_counts = sorted(self.branches.values(), key=lambda b: b.visit_count)
+        remaining_rounds = num_rounds - self.total_visit_count
+        return visit_counts[-1].visit_count >= visit_counts[-2].visit_count + remaining_rounds
 
 
 class MCTS:
