ee88710162e335ffa03b670e0ed0683daabdc3b6
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-07-26 14:27:15 -0600

    Add function to Node; Update docstrings

--- botfjord/mcts.py
+++ botfjord/mcts.py
@@ -56,6 +56,11 @@ class Node:
         """Returns bool for existence of given child node"""
         return move in self.children
 
+    def get_child(self, move: Move):
+        """Returns child node for given move\n
+        Raise exception if child does not exist"""
+        return self.children[move]
+
     def expected_value(self, move: Move):
         """Returns expected value for given move\n
         Expected value is the average value of past visits to that branch"""
@@ -65,18 +70,23 @@ class Node:
         return branch.total_value / branch.visit_count
 
     def prior(self, move: Move):
+        """Returns prior value for given move"""
         return self.branches[move].prior
 
     def visit_count(self, move: Move):
+        """Returns visit count of branch for given move"""
         branch = self.branches.get(move)
         return branch.visit_count if branch is not None else 0
 
     def record_visit(self, move: Move, value: float):
+        """Updates the branch for given move"""
         self.branches[move].visit_count += 1
         self.branches[move].total_value += value
         self.total_visit_count += 1
 
     def check_visit_counts(self, num_rounds: int):
+        """Returns True if the most visited branch is guaranteed to be selected
+        for the given number of search rounds"""
         visit_counts = sorted(self.branches.values(), key=lambda b: b.visit_count)
         remaining_rounds = num_rounds - self.total_visit_count
         return visit_counts[-1].visit_count >= visit_counts[-2].visit_count + remaining_rounds
