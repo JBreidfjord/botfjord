51bff7d0ea8fca1e4af93d5c57008396c428ea57
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-08-01 19:25:42 -0600

    Add threading

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,16 @@ use crate::{
     eval::Evaluator,
     mcts::{Limit, Tree},
 };
-use chess::{ChessMove, Game};
+use chess::{Board, ChessMove, MoveGen};
 use ordered_float::OrderedFloat;
 use pyo3::prelude::*;
-use std::str::FromStr;
+use std::{
+    collections::HashMap,
+    str::FromStr,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+    time::Instant,
+};
 
 mod eval;
 mod mcts;
@@ -29,17 +35,78 @@ fn uci(action: &ChessMove) -> String {
 }
 
 #[pyfunction]
-fn search_tree(fen: String) -> String {
-    let game = Game::from_str(&fen).unwrap();
+fn search_tree(fen: String, time: f32, temperature: f32, processes: usize) -> String {
+    let start = Instant::now();
 
-    let evaluator = Evaluator::new();
-    let mut tree = Tree::new(evaluator, 1.41, 0.3);
-    let limit = Limit::new(Some(30.0), Some(250_000.0));
+    let mut handles = vec![];
+    let mut move_dict = HashMap::new();
 
-    let mut results = tree.search(game.current_position(), Some(limit));
-    results.sort_by_key(|x| OrderedFloat(x.1));
-    let result = results.last().unwrap();
-    uci(&result.0)
+    for action in MoveGen::new_legal(&Board::from_str(&fen).unwrap()) {
+        move_dict.insert(action, 0);
+    }
+
+    let fen_mtx = Arc::new(Mutex::new(fen));
+    let time_mtx = Arc::new(Mutex::new(time));
+    let temperature_mtx = Arc::new(Mutex::new(temperature));
+
+    let (tx, rx) = mpsc::channel();
+    let tx_mtx = Arc::new(Mutex::new(tx));
+
+    for _ in 0..processes {
+        let t_fen = Arc::clone(&fen_mtx);
+        let t_time = Arc::clone(&time_mtx);
+        let t_temperature = Arc::clone(&temperature_mtx);
+        let t_tx = Arc::clone(&tx_mtx);
+
+        let handle = thread::spawn(move || {
+            let board = Board::from_str(&t_fen.lock().unwrap()).unwrap();
+
+            let evaluator = Evaluator::new();
+            let mut tree = Tree::new(evaluator, *t_temperature.lock().unwrap(), 0.3);
+            let limit = Limit::new(Some(*t_time.lock().unwrap()), Some(0.0));
+
+            let results = tree.search(board, limit);
+            for result in results {
+                t_tx.lock().unwrap().send(result).unwrap();
+            }
+        });
+        handles.push(handle);
+    }
+
+    drop(tx_mtx);
+    for (action, visits) in rx {
+        *move_dict.get_mut(&action).unwrap() += visits as usize;
+    }
+
+    let mut results = vec![];
+    for item in move_dict.iter() {
+        results.push(item);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    results.sort_by_key(|x| x.1);
+    results.reverse();
+    let mut fmt_results = vec![];
+    let mut nodes = 0;
+    for (i, (action, value)) in results.iter().enumerate() {
+        nodes += **value;
+        if i < 5 {
+            fmt_results.push(format!("{} {:.0}", uci(action), value));
+        }
+    }
+    let run_time = start.elapsed().as_secs_f32();
+    println!(
+        "{} | {:.0} nodes/s ({:.2}s | {:.0} nodes)",
+        fmt_results.join(" | "),
+        nodes as f32 / run_time,
+        run_time,
+        nodes
+    );
+
+    uci(&results[0].0)
 }
 
 #[pymodule]
