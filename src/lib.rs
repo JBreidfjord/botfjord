d8436c9415d74d2be5a70762306765b6fd82be14
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-15 16:29:38 -0700

    Replace mutexes with cloned data

--- src/lib.rs
+++ src/lib.rs
@@ -38,42 +38,36 @@ fn uci(action: &ChessMove) -> String {
 fn search_tree(fen: String, time: f32, temperature: f32, processes: usize) -> String {
     let start = Instant::now();
 
-    let mut handles = vec![];
-    let mut move_dict = HashMap::new();
+    let mut handles = Vec::new();
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..processes {
+        let thread_temp = temperature.clone();
+        let thread_time = time.clone();
+        let thread_fen = fen.clone();
+        let thread_tx = tx.clone();
 
+        let handle = thread::spawn(move || {
+            let board = Board::from_str(&thread_fen).unwrap();
+
+            let evaluator = Evaluator::new();
+            let mut tree = Tree::new(evaluator, thread_temp, 0.3);
+            let limit = Limit::new(Some(thread_time), Some(0.0));
+
+            let results = tree.search(board, limit);
+            for result in results {
+                thread_tx.send(result).unwrap();
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    let mut move_dict = HashMap::new();
     for action in MoveGen::new_legal(&Board::from_str(&fen).unwrap()) {
         move_dict.insert(action, 0);
     }
 
-    let fen_mtx = Arc::new(Mutex::new(fen));
-    let time_mtx = Arc::new(Mutex::new(time));
-    let temperature_mtx = Arc::new(Mutex::new(temperature));
-
-    let (tx, rx) = mpsc::channel();
-    let tx_mtx = Arc::new(Mutex::new(tx));
-
-    for _ in 0..processes {
-        let t_fen = Arc::clone(&fen_mtx);
-        let t_time = Arc::clone(&time_mtx);
-        let t_temperature = Arc::clone(&temperature_mtx);
-        let t_tx = Arc::clone(&tx_mtx);
-
-        let handle = thread::spawn(move || {
-            let board = Board::from_str(&t_fen.lock().unwrap()).unwrap();
-
-            let evaluator = Evaluator::new();
-            let mut tree = Tree::new(evaluator, *t_temperature.lock().unwrap(), 0.3);
-            let limit = Limit::new(Some(*t_time.lock().unwrap()), Some(0.0));
-
-            let results = tree.search(board, limit);
-            for result in results {
-                t_tx.lock().unwrap().send(result).unwrap();
-            }
-        });
-        handles.push(handle);
-    }
-
-    drop(tx_mtx);
+    drop(tx);
     for (action, visits) in rx {
         *move_dict.get_mut(&action).unwrap() += visits as usize;
     }
